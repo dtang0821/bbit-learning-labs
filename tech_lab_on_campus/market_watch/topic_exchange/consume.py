4aae0356b8e65894bfe728ec8ebecc81cec59e76
ldiaz16 <112440062+ldiaz16@users.noreply.github.com>
2026-02-25 23:44:16 +0000

    Implemented consume.py

--- tech_lab_on_campus/market_watch/topic_exchange/consume.py
+++ tech_lab_on_campus/market_watch/topic_exchange/consume.py
@@ -19,21 +19,30 @@ from solution.consumer_sol import mqConsumer  # pylint: disable=import-error
 
 def main(sector: str, queueName: str) -> None:
     
-    # Implement Logic to Create Binding Key from the ticker and sector variable -  Step 2
-    #
-    #                       WRITE CODE HERE!!!
-    #
+    bindingKey= f"*.*.{sector}"
+    bindingKey.strip()
     
     consumer = mqConsumer(binding_key=bindingKey,exchange_name="Tech Lab Topic Exchange",queue_name=queueName)    
     consumer.startConsuming()
     
 
-
 if __name__ == "__main__":
 
     # Implement Logic to read the sector and queueName string from the command line and save them - Step 1
-    #
-    #                       WRITE CODE HERE!!!
-    #
+    parser = argparse.ArgumentParser(
+        description="Process Stock Sector, And Queue Name."
+    )
+    parser.add_argument(
+        "-s",
+        "--sector",
+        type=str,
+        help="Stock Sectors",
+        required=True,
+    )
 
-    sys.exit(main(sector,queue))
+    parser.add_argument(
+        "-q", "--queue", type=str, help="Queue Name", required=True
+    )
+    args = parser.parse_args()
+
+    sys.exit(main(args.sector,args.queue))
